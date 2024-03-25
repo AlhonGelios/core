b4287bf9f3bd4e07c3941a39d7afcd62d3e9aa2e
Andrey <geliosal39hon@gmail.com>
2024-03-25 09:01:05 +0300

    feat: auth add role

--- src/widgets/app-header/_ui/profile.tsx
+++ src/widgets/app-header/_ui/profile.tsx
@@ -12,7 +12,7 @@ import { LogOut, User } from "lucide-react";
 import { Button } from "@/shared/ui/button";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui/avatar";
-import { useAppSession } from "@/entities/user/use-app-session";
+import { useAppSession, useRole } from "@/entities/user/session.client";
 import { Skeleton } from "@/shared/ui/skeleton";
 import { useSignOut } from "@/features/auth/use-sign-out";
 import { SignInButton } from "@/features/auth/sign-in-button";
@@ -21,7 +21,7 @@ export function Profile() {
   const session = useAppSession();
   const { signOut, isPending: isLoadingSignOut } = useSignOut();
 
-  console.log(session.data);
+  console.log(useRole());
 
   if (session.status === "loading") {
     return <Skeleton className="w-8 h-8 rounded-full" />;
