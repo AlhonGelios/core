56b111a4fa200a8546fb32da1b12c305bf06dc80
Andrey <geliosal39hon@gmail.com>
2024-09-24 20:02:33 +0300

    feat: upload-avatar instanceof Blob

--- src/features/update-profile/_actions/upload-avatar.ts
+++ src/features/update-profile/_actions/upload-avatar.ts
@@ -12,7 +12,7 @@ const resultSchema = z.object({
 export const uploadAvatarAction = async (formData: FormData) => {
   const file = formData.get(AVATAR_FILE_KEY);
 
-  if (!(file instanceof File)) {
+  if (!(file instanceof Blob)) {
     throw new BadRequest();
   }
 
