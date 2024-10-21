13c0bdda77b39da2f9570f1facbb3f649e2da0ac
Andrey <geliosal39hon@gmail.com>
2024-10-21 12:40:33 +0300

    chor: work next-auth

--- src/kernel/lib/next-auth/server.ts
+++ src/kernel/lib/next-auth/server.ts
@@ -1,6 +1,7 @@
 import { ContainerModule } from "inversify";
 import { SessionService } from "./_session-service";
 import { NextAuthConfig } from "./_next-auth-config";
+export { CreateUserService } from "./_create-user-service";
 
 export const NextAuthModule = new ContainerModule((bind) => {
   bind(NextAuthConfig).toSelf();
