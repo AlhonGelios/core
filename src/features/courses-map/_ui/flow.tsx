4fc61241dc37f9dac1de8c27c926b827320a1686
Andrey <geliosal39hon@gmail.com>
2024-10-22 18:35:11 +0300

    feat: underway

---
+++ src/features/courses-map/_ui/flow.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export function Flow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  return (
+    <div className="fixed inset-0 flex flex-col">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      />
+    </div>
+  );
+}
---
+++ src/features/courses-map/courses-map.tsx
@@ -0,0 +1,9 @@
+import { Flow } from "./_ui/flow";
+
+export function CoursesMap() {
+  return (
+    <div className="grow relative">
+      <Flow />
+    </div>
+  );
+}
